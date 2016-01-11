06d471f58fb0d8d23974c11fd1a4c759c3c5e8f2
Jacob Sims <jacobsims@users.noreply.github.com>
2016-01-10 21:29:04 -0500

    Add second and third place to summary graphic

--- imagemaker.py
+++ imagemaker.py
@@ -2,11 +2,15 @@
 import os
 
 template = '''<?xml version="1.0"?>
-<svg width="{width}" height="105" viewBox="0 0 {width} 105" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <rect x="0" y="0" rx="5" ry="5" width="{width}" height="105" style="fill: #333333" />
+<svg width="{width}" height="205" viewBox="0 0 {width} 205" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <rect x="0" y="0" rx="5" ry="5" width="{width}" height="205" style="fill: #333333" />
     <text x="15" y="35" font-family="Courier, monospace" font-size="27" fill="#ffffff">Darby Robocode Battle {battlenum}</text>
     <text x="15" y="60" font-family="Courier, monospace" font-size="15" fill="#aaaaaa">Won by:</text>
     <text x="25" y="85" font-family="Courier, monospace" font-size="20" fill="#ffffff">{winner}</text>
+    <text x="15" y="110" font-family="Courier, monospace" font-size="15" fill="#aaaaaa">2nd:</text>
+    <text x="25" y="135" font-family="Courier, monospace" font-size="20" fill="#ffffff">{second}</text>
+    <text x="15" y="160" font-family="Courier, monospace" font-size="15" fill="#aaaaaa">3rd:</text>
+    <text x="25" y="185" font-family="Courier, monospace" font-size="20" fill="#ffffff">{third}</text>
 </svg>
 '''
 
@@ -14,11 +18,16 @@ with open('results-columns.txt', 'r') as f:
     leaderboardLines = str(f.read()).splitlines()
 
 winner = '[error finding winner]'
+second = '[no more bots]'
+third = '[no more bots]'
 for line in leaderboardLines:
     if line.startswith('1st:'):
         winner = line[5:line.index(' ', 5)]
+    if line.startswith('2nd:'):
+        second = line[5:line.index(' ', 5)]
+    if line.startswith('3rd:'):
+        third = line[5:line.index(' ', 5)]
 
-width = max(440, 63 + int(len(winner) * 11.7))
+width = max(440, 63 + int(max(len(winner), len(second), len(third)) * 11.7))
 
-
-print template.format(width=width, winner=winner, battlenum=os.environ['CIRCLE_BUILD_NUM'])
+print template.format(width=width, winner=winner, second=second, third=third, battlenum=os.environ['CIRCLE_BUILD_NUM'])
