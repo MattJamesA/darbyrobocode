d575cd67f851dfe25ceb861fb8e0e53c1c6f8132
Jacob Sims <jacobsims@users.noreply.github.com>
2016-01-10 20:01:16 -0500

    Remove links from SVG, as they dont work on GitHub

--- imagemaker.py
+++ imagemaker.py
@@ -2,18 +2,11 @@
 import os
 
 template = '''<?xml version="1.0"?>
-<svg width="{width}" height="130" viewBox="0 0 {width} 130" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <rect x="0" y="0" rx="5" ry="5" width="{width}" height="130" style="fill: #333333" />
+<svg width="{width}" height="105" viewBox="0 0 {width} 105" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <rect x="0" y="0" rx="5" ry="5" width="{width}" height="105" style="fill: #333333" />
     <text x="15" y="35" font-family="Courier, monospace" font-size="27" fill="#ffffff">Darby Robocode Battle {battlenum}</text>
     <text x="15" y="60" font-family="Courier, monospace" font-size="15" fill="#aaaaaa">Won by:</text>
     <text x="25" y="85" font-family="Courier, monospace" font-size="20" fill="#ffffff">{winner}</text>
-    <a xlink:href="https://darbycomputerclub.github.io/darbyrobocode/results-columns.txt">
-        <text x="15" y="110" font-family="Courier, monospace" font-size="15" fill="#cccccc">see leaderboard</text>
-    </a>
-    <text x="165" y="110" font-family="Courier, monospace" font-size="15" fill="#aaaaaa">|</text>
-    <a xlink:href="https://darbycomputerclub.github.io/darbyrobocode/darbybattle.br">
-        <text x="187" y="110" font-family="Courier, monospace" font-size="15" fill="#cccccc">download record</text>
-    </a>
 </svg>
 '''
 
